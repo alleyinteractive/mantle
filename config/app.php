d87492496e3cf8450af498e6d7dacbece7e37c0d
Sean Fisher <srtfisher@gmail.com>
2020-05-08 17:22:39 -0400

    Setting aliases in the config

--- config/app.php
+++ config/app.php
@@ -12,4 +12,12 @@ return [
 	'providers' => [
 		Mantle\Framework\Database\Factory_Service_Provider::class,
 	],
+
+	/**
+	 * Application Aliases
+	 */
+	'aliases'   => [
+		'App'    => Mantle\Framework\Facade\App::class,
+		'Config' => Mantle\Framework\Facade\Config::class,
+	],
 ];
