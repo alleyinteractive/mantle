d2ee7ff287c1e1f0daba641db61a0a8e44a997b1
Sean Fisher <srtfisher@gmail.com>
2020-05-11 22:37:45 -0400

    Adding debug item to config

---
+++ config/local/app.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Application Configuration
+ *
+ * @package Mantle
+ */
+
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Application Debug Mode
+	|--------------------------------------------------------------------------
+	|
+	| Enabled by default for local development.
+	|
+	*/
+	'debug' => true,
+];
