a396c0f9faa2e74b1dd5a1883c66bcb187e320da
Eric Fuller <eric@alley.co>
2020-05-20 12:34:30 -0400

    Default category factory.

---
+++ database/factories/category-factory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Category Factory
+ *
+ * @package Mantle
+ */
+
+// phpcs:disable Squiz.Commenting.FunctionComment.MissingParamComment
+
+namespace App\Factory;
+
+use Faker\Generator as Faker;
+use Mantle\Framework\Database\Model\Term;
+
+/**
+ * Factory definition.
+ *
+ * @var \Mantle\Framework\Database\Factory\Factory $factory
+ */
+$factory->define(
+	Term::class, // phpcs:ignore
+	function ( Faker $faker ) {
+		return [
+			'name'        => $faker->sentence,
+			'description' => $faker->paragraph,
+			'taxonomy'    => 'category',
+			'object_type' => 'post',
+		];
+	}
+);
