3072269d38f9c2e84803eba5efa4117493f3b3ea
Sean Fisher <srtfisher@gmail.com>
2020-05-28 12:19:27 -0400

    Revert "Revert "Feature: Router""

--- config/app.php
+++ config/app.php
@@ -34,8 +34,9 @@ return [
 		Mantle\Framework\Providers\Console_Service_Provider::class,
 
 		// Application Providers.
-		App\Providers\Model_Register_Provider::class,
 		App\Providers\App_Service_Provider::class,
+		App\Providers\Model_Register_Provider::class,
+		App\Providers\Route_Service_Provider::class,
 	],
 
 	/*
@@ -48,9 +49,10 @@ return [
 	|
 	 */
 	'aliases'   => [
-		'App'    => Mantle\Framework\Facade\App::class,
-		'Config' => Mantle\Framework\Facade\Config::class,
-		'Log'    => Mantle\Framework\Facade\Log::class,
+		'App'     => Mantle\Framework\Facade\App::class,
+		'Config'  => Mantle\Framework\Facade\Config::class,
+		'Log'     => Mantle\Framework\Facade\Log::class,
+		'Request' => Mantle\Framework\Facade\Request::class,
 	],
 
 	/*
