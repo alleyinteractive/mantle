c262f968446014a7e4ab24b10d3fd2ac2d689ff2
Sean Fisher <srtfisher@gmail.com>
2020-06-03 17:05:09 -0400

    Queue config

---
+++ config/queue.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Queue Configuration
+ *
+ * @package Mantle
+ */
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Queue Provider
+	|--------------------------------------------------------------------------
+	|
+	| Define the queue provider used in the application.
+	|
+	*/
+	'default' => 'wordpress',
+];
