632e6dbea2512e372e8e3b54b1f69119c0830fb5
Sean Fisher <srtfisher@gmail.com>
2020-07-14 17:12:28 -0400

    Adding the base seeder

---
+++ database/seeds/class-database-seeder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Database_Seeder class file.
+ *
+ * @package Mantle
+ */
+
+namespace App\Database\Seeds;
+
+use Mantle\Framework\Database\Seeder;
+use function Mantle\Framework\Helpers\factory;
+
+/**
+ * Application Seeder
+ */
+class Database_Seeder extends Seeder {
+	/**
+	 * Run the seeder.
+	 */
+	public function run() {
+		// Call additional seeders...
+	}
+}
