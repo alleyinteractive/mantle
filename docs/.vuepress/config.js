9c87852b4cfbc029576760cfbfe8e57d07b59f9c
Sean Fisher <srtfisher@gmail.com>
2020-09-14 23:10:13 -0400

    fixing comma

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,7 +45,7 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'robots', content: 'noindex' }]
   ],
   themeConfig: {
