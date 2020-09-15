0af1b3e19a7312913dbb41ee1cf24e22b8238895
Sean Fisher <srtfisher@gmail.com>
2020-09-14 22:51:17 -0400

    Adding in vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,37 @@
+const glob = require('glob');
 const { description } = require('../package')
+
+// Convert text to have uppercase first characters.
+const slugToTitle = (text) => {
+  const parts = text.split('-');
+  parts.shift();
+
+  return parts
+    .join(' ')
+    .replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
+    .replace('.md', '');
+};
+
+const sidebar = glob
+  .sync('./!(vendor|node_modules)')
+  .map((folder) => {
+    const files = glob.sync(`${folder}/*.md`);
+    if (! files.length) {
+      return false;
+    }
+
+    return {
+      title: slugToTitle(folder),
+      path: files[0].replace('.md', '').replace('./', '/'),
+      collapsable: false,
+      sidebarDepth: 1,
+      children: files.map((file) => ({
+        title: slugToTitle(file.split('/').pop()),
+        path: file.replace('.md', '').replace('./', '/'),
+      })),
+    };
+  })
+  .filter(item => false !== item);
 
 module.exports = {
   /**
@@ -8,63 +41,36 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
-  description: description,
-
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
+  description,
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
+    docsBranch: 'main',
+    docsDir: 'docs',
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    lastUpdated: 'Last Updated',
+    repo: 'alleyinteractive/mantle-site',
     nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
+      { text: 'Home', link: '/', target: '_self', },
+      { text: 'Alley', link: 'https://alley.co/', },
     ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
+    sidebar,
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-clean-urls',
+      {
+        normalSuffix: '/',
+        indexSuffix: '/',
+        notFoundPath: '/404.html',
+      },
+    ],
   ]
 }
