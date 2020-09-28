47416ea8d47402891847c149130b469a94265ec5
Sean Fisher <srtfisher@gmail.com>
2020-09-28 16:59:48 -0400

    Adding config for blade views

---
+++ config/view.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * View Configuration
+ *
+ * @package Mantle
+ */
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Compiled View Path
+	|--------------------------------------------------------------------------
+	|
+	| This option determines where all the compiled Blade templates will be
+	| stored for your application.
+	*/
+
+	'compiled' => storage_path( 'framework/views' ),
+];
