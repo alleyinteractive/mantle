d4cc4503208b31b8600a341ac70f5d12eaa9b4b8
Sean Fisher <srtfisher@gmail.com>
2020-10-26 14:28:11 -0400

    phpcs

--- config/filesystem.php
+++ config/filesystem.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Filesystem Configuration
+ *
+ * @package Mantle
  */
 
 return [
@@ -15,7 +17,6 @@ return [
 	| based disks are available to your application. Just store away!
 	|
 	*/
-
 	'default' => 'local',
 
 
@@ -33,43 +34,26 @@ return [
 	*/
 
 	'disks' => [
+		'local' => [
+			'driver' => 'local',
+			'root' => storage_path( 'app' ),
+		],
 
-			'local' => [
-					'driver' => 'local',
-					'root' => storage_path('app'),
-			],
+		'public' => [
+			'driver'     => 'local',
+			'root'       => storage_path( 'app/public' ),
+			'url'        => env( 'APP_URL' ) . '/storage',
+			'visibility' => 'public',
+		],
 
-			'public' => [
-					'driver' => 'local',
-					'root' => storage_path('app/public'),
-					'url' => env('APP_URL').'/storage',
-					'visibility' => 'public',
-			],
-
-			's3' => [
-					'driver' => 's3',
-					'key' => env('AWS_ACCESS_KEY_ID'),
-					'secret' => env('AWS_SECRET_ACCESS_KEY'),
-					'region' => env('AWS_DEFAULT_REGION'),
-					'bucket' => env('AWS_BUCKET'),
-					'url' => env('AWS_URL'),
-					'endpoint' => env('AWS_ENDPOINT'),
-			],
-
-	],
-
-	/*
-	|--------------------------------------------------------------------------
-	| Symbolic Links
-	|--------------------------------------------------------------------------
-	|
-	| Here you may configure the symbolic links that will be created when the
-	| `storage:link` Artisan command is executed. The array keys should be
-	| the locations of the links and the values should be their targets.
-	|
-	*/
-
-	'links' => [
-			// public_path('storage') => storage_path('app/public'),
+		's3' => [
+			'driver'   => 's3',
+			'key'      => env( 'AWS_ACCESS_KEY_ID' ),
+			'secret'   => env( 'AWS_SECRET_ACCESS_KEY' ),
+			'region'   => env( 'AWS_DEFAULT_REGION' ),
+			'bucket'   => env( 'AWS_BUCKET' ),
+			'url'      => env( 'AWS_URL' ),
+			'endpoint' => env( 'AWS_ENDPOINT' ),
+		],
 	],
 ];
