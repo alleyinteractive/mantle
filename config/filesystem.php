5c80f5d3dd125c5479b4cd47505b8b9eb0b44484
Sean Fisher <srtfisher@gmail.com>
2020-10-26 22:37:05 -0400

    Updating config

--- config/filesystem.php
+++ config/filesystem.php
@@ -38,12 +38,10 @@ return [
 
 		's3' => [
 			'driver'   => 's3',
-			'key'      => env( 'AWS_ACCESS_KEY_ID' ),
-			'secret'   => env( 'AWS_SECRET_ACCESS_KEY' ),
-			'region'   => env( 'AWS_DEFAULT_REGION' ),
-			'bucket'   => env( 'AWS_BUCKET' ),
-			'url'      => env( 'AWS_URL' ),
-			'endpoint' => env( 'AWS_ENDPOINT' ),
+			'key'      => '',
+			'secret'   => '',
+			'region'   => 'us-east-2',
+			'bucket'   => '',
 		],
 	],
 ];
