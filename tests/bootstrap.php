795e718de5a5bde1fb6aef54a50122ab35046abd
Sean Fisher <srtfisher@gmail.com>
2020-10-30 17:03:03 -0400

    Try loading mantle.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,16 @@
 namespace App\Tests;
 
 // Install the Mantle Test Framework.
-\Mantle\Framework\Testing\install();
+\Mantle\Framework\Testing\install(
+	function() {
+		\Mantle\Framework\Testing\tests_add_filter(
+			'muplugins_loaded',
+			function() {
+				require_once __DIR__ . '/../mantle.php';
+			}
+		);
+	}
+);
 
 spl_autoload_register(
 	\Mantle\Framework\generate_wp_autoloader( __NAMESPACE__, __DIR__ )
