26fb4c4a3a570bb186bb34b8453950c1a5e6fe4b
Sean Fisher <srtfisher@gmail.com>
2020-11-24 13:33:32 -0500

    Google site verify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   description,
   base: '/',
   head: [
-    // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'google-site-verification', content: '9j6GWEdJJsL1zqzPRBMYahbaFg0NNj-NVglppOfGyJE' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ],
