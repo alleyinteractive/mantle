833173036944ba053fbf3b233a0b5bb73a68e49b
Sean Fisher <srtfisher@gmail.com>
2021-02-22 22:18:54 -0500

    Fixing contracts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,39 +5,40 @@
  * @package Mantle
  */
 
-use Mantle\Framework;
+use Mantle\Contracts;
+use Mantle\Framework\Application;
 use Mantle\Http\Request;
 
 /**
  * Instantiate the application
  */
-$app = new Framework\Application();
+$app = new Application();
 
 /**
  * Register the main contracts that power the application.
  */
 $app->singleton(
-	Framework\Contracts\Console\Kernel::class,
+	Contracts\Console\Kernel::class,
 	App\Console\Kernel::class,
 );
 
 $app->singleton(
-	Framework\Contracts\Http\Kernel::class,
+	Contracts\Http\Kernel::class,
 	App\Http\Kernel::class,
 );
 
 $app->singleton(
-	Framework\Contracts\Exceptions\Handler::class,
+	Contracts\Exceptions\Handler::class,
 	App\Exceptions\Handler::class
 );
 
 // Load the Application's Kernel depending on the context it was called.
 if ( defined( 'WP_CLI' ) && \WP_CLI ) {
-	$app_kernel = $app->make( Framework\Contracts\Console\Kernel::class );
+	$app_kernel = $app->make( Contracts\Console\Kernel::class );
 	$app_kernel->handle();
 } else {
 	// Boot up the HTTP Kernel.
-	$app_kernel = $app->make( Framework\Contracts\Http\Kernel::class );
+	$app_kernel = $app->make( Contracts\Http\Kernel::class );
 	$app_kernel->handle( Request::capture() );
 }
 
