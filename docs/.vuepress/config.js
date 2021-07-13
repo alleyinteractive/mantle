485f0fd0495d4b47c98a640785713c38d000cfa4
Sean Fisher <srtfisher@gmail.com>
2021-07-12 23:20:18 -0400

    Adding a dark theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { property: 'og:image', content: 'https://repository-images.githubusercontent.com/261240189/e61bc280-2d73-11eb-92d0-249447854ca0' }],
   ],
+  theme: 'default-prefers-color-scheme',
   themeConfig: {
     activeHeaderLinks: false,
     displayAllHeaders: false,
