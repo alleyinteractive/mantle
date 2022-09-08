4a6c197c6ff0e700fb4bb653da46e91c1c236adc
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-09-08 13:13:52 -0400

    Add Block Service Provider to default service providers

--- config/app.php
+++ config/app.php
@@ -35,6 +35,7 @@ return [
 		Mantle\Framework\Providers\Error_Service_Provider::class,
 		Mantle\Framework\Providers\Model_Service_Provider::class,
 		Mantle\Framework\Providers\Queue_Service_Provider::class,
+		Mantle\Framework\Providers\Block_Service_Provider::class,
 		Mantle\Query_Monitor\Query_Monitor_Service_Provider::class,
 		Mantle\Framework\Providers\New_Relic_Service_Provider::class,
 		Mantle\Database\Pagination\Paginator_Service_Provider::class,
