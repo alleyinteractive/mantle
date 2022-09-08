246441de4ec70e6223ff3045b3aceefd53fca22b
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-09-08 13:31:52 -0400

    Fix PHPCS issues

--- app/blocks/class-test-block.php
+++ app/blocks/class-test-block.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Test Block
+ *
  * @package Mantle
  */
 
@@ -9,13 +10,32 @@ namespace App\Blocks;
 use Mantle\Blocks\Block;
 use Mantle\Contracts\Block as Block_Contract;
 
+/**
+ * An example of how to register a block with Mantle.
+ */
 class Test_Block extends Block implements Block_Contract {
 
-	protected string $namespace = 'app';
-
+	/**
+	 * The name of the block.
+	 *
+	 * @var string
+	 */
 	protected string $name = 'test';
 
-	public function register(){
+	/**
+	 * The namespace of the block.
+	 *
+	 * @var string
+	 */
+	protected string $namespace = 'app';
+
+	/**
+	 * Executed by the Block Service Provider to handle registering the block
+	 * with Mantle and WordPress.
+	 *
+	 * @return void
+	 */
+	public function register(): void {
 		$this->editor_script = mix( 'blocks/test/index.js' );
 
 		parent::register();
