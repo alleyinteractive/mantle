19cc528450150ea3cc861e465a1b6e398195007a
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-09-08 13:32:32 -0400

    Add example of using Blade for block template

--- views/blocks/app/test.blade.php
+++ views/blocks/app/test.blade.php
@@ -1,1 +1,5 @@
-This is an example block! Nothing to see here!
+<p>This is an example of a block using a blade template.</p>
+
+@for ($i = 0; $i < 10; $i++)
+    <p>The current value is {{ $i }}</p>
+@endfor
