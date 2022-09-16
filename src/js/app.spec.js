b5486127ed5eee4b0219b0d5a6b25826f676e2fa
Sean Fisher <srtfisher@gmail.com>
2022-09-16 14:49:58 -0400

    Adding types/jest

---
+++ src/js/app.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Example Jest Test
+ *
+ * @see https://jestjs.io/docs/getting-started
+ */
+
+describe('app', () => {
+  it('should perform a test', () => {
+    expect(true).toEqual(true);
+  });
+});
