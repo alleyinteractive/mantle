9f169523380b759c187d122cd18fd94c440e3f6a
Sean Fisher <srtfisher@gmail.com>
2023-02-17 13:43:36 -0500

    Renaming example entry

---
+++ entries/example-entry/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Example entry point to enqueue.
+ *
+ * Entries are automatically built with webpack when included inside the
+ * ../entries directory. Enqueue in an entry index.php file or in src/assets.php
+ */
+
+// Get started!
