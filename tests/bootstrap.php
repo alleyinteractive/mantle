6d72cb0d0bcebf821b62bd2c1b0ea4e2f3fae97e
Sean Fisher <srtfisher@gmail.com>
2023-08-03 15:18:09 -0400

    Use a working version of phpcs for 8.1+

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,6 @@ namespace App\Tests;
 
 // Install the Mantle Test Framework.
 \Mantle\Testing\manager()
+	->with_sqlite()
 	->loaded( fn () => require_once __DIR__ . '/../mantle.php' )
 	->install();
