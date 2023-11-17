db9b6b2e688b5aa4915eb1ba5145f7a5ea72d7e3
Sean Fisher <srtfisher@gmail.com>
2023-11-17 11:08:36 -0500

    Adding new configuration for queue work (#198)

--- config/queue.php
+++ config/queue.php
@@ -29,6 +29,39 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
+	| Maximum number of concurrent batches
+	|--------------------------------------------------------------------------
+	|
+	| The maximum number of batches that can be run concurrently. For example,
+	| if 1000 queue jobs are dispatched and this is set to 5 with a batch size
+	| of 100, then 5 batches of 100 will be run concurrently and take two runs
+	| of the queue to complete.
+	|
+	*/
+	'max_concurrent_batches' => environment( 'QUEUE_MAX_CONCURRENT_BATCHES', 1 ),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Delete failed or processed queue items after a set time
+	|--------------------------------------------------------------------------
+	|
+	| Delete failed or processed queue items after a set time in seconds.
+	|
+	*/
+	'delete_after' => environment( 'QUEUE_DELETE_AFTER', 60 * 60 * 24 * 7 ),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Enable the Queue Admin Interface
+	|--------------------------------------------------------------------------
+	|
+	| Enable the queue admin interface to display queue jobs.
+	|
+	*/
+	'enable_admin' => environment( 'QUEUE_ENABLE_ADMIN', true ),
+
+	/*
+	|--------------------------------------------------------------------------
 	| Provider Configuration
 	|--------------------------------------------------------------------------
 	|
